4531cc27c7d9fed382544fe95dd471fb69838e51
Natasha Wolfe <natasha.c.wolfe@gmail.com>
2020-03-03 16:41:08 -0800

    added action prompts to start

--- cms.js
+++ cms.js
@@ -15,4 +15,27 @@ var connection = mysql.createConnection({
 connection.connect((err) => {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
+    startCMS();
 });
+
+function startCMS() {
+    inquirer
+    .prompt({
+        name: 'action',
+        type: 'rawlist',
+        message: 'Welcome to the CMS Employee Tracker! What can we assist with today?',
+        choices: [
+            'View All Employees',
+            'View All Employees By Department', 
+            'View All employees By Role',
+            'Add Employeee',
+            'Remove Employee',
+            'Update Employee Role',
+            'Update Deparment',
+            'Update Role',
+        ]
+    })
+    .then(function(answer) {
+        console.log(answer);
+    })
+}
--- seeds.sql
+++ seeds.sql
@@ -1,7 +1,7 @@
 INSERT INTO department (name)
 VALUES ("sales"), ("engineering"), ("finance"), ("legal");
 
-INSERT INTO roles (title, salary)
-VALUES ("sales lead", 120000), ("sales person", 80000), ("lead engineer", 150000), 
-("software engineer", 100000), ("lead accountant", 100000), ("accountant", 80000), 
-("legal team lead", 150000), ("lawyer", 120000);
+INSERT INTO role (title, salary, department_FK)
+VALUES ("sales lead", 120000, 1), ("sales person", 80000, 1), ("lead engineer", 150000, 2), 
+("software engineer", 100000, 2), ("lead accountant", 100000, 3), ("accountant", 80000, 3), 
+("legal team lead", 150000, 4), ("lawyer", 120000, 4);
