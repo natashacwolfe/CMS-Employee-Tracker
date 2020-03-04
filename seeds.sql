4bf134ecc1489284e498d8754087a02fa3f6a11f
Natasha Wolfe <natasha.c.wolfe@gmail.com>
2020-03-03 16:20:50 -0800

    added seeds file with departments and roles

---
+++ seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO department (name)
+VALUES ("sales"), ("engineering"), ("finance"), ("legal");
+
+INSERT INTO roles (title, salary)
+VALUES ("sales lead", 120000), ("sales person", 80000), ("lead engineer", 150000), 
+("software engineer", 100000), ("lead accountant", 100000), ("accountant", 80000), 
+("legal team lead", 150000), ("lawyer", 120000);
